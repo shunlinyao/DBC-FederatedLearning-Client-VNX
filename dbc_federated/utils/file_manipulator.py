3065a2a6064c87ef7c74ccba8004befbd7bc329a
shunlinyao <yaoshunlin@yahoo.com>
2019-07-31 00:48:31 -0700

    new merge method

--- dbc_federated/utils/file_manipulator.py
+++ dbc_federated/utils/file_manipulator.py
@@ -5,6 +5,9 @@ import glob
 import random
 import copy
 import shutil
+import errno
+import pathlib
+from pathlib import Path
 from shutil import copyfile
 
 
@@ -112,3 +115,57 @@ def refolder(data_folder, targ_folder, train_fraction=0.8, val_fraction=0.2, tes
         
         if remove_original==True:
             shutil.rmtree(data_folder)
+			
+#merge trees into one		
+out_folder = '/mnt/d/temp/test1/merge'
+input_paths = ['/mnt/d/temp/test1/in/in1','/mnt/d/temp/test1/in/in2','/mnt/d/temp/test1/in/in3']
+
+def merge(input_paths, out_folder):	
+	filelist=[]
+	foldername=str(os.path.basename(input_paths[0]))
+	tempin=str(input_paths[0])
+	tempout=out_folder
+	#copytree
+	try:
+		shutil.copytree(tempin, tempout)
+	except OSError as e:
+        # If the error was caused because the source wasn't a directory
+		if e.errno == errno.ENOTDIR:
+			shutil.copy(tempin, tempout)
+		else:
+			print('Directory not copied. Error: %s' % e)
+			
+	#clean folder		
+	for dirpath, dirnames, filenames in os.walk(out_folder):
+		
+		for filename1 in filenames:
+			os.remove(str(dirpath + '/' + filename1))
+			
+	#copy files by name		
+	for dirpath, dirnames, filenames in os.walk(Path(tempin).parent):
+		filelist=filelist+filenames
+		for filename in filenames:
+			commonpath=os.path.commonpath([dirpath, out_folder]) + '/in'
+			p = pathlib.Path(dirpath.replace(commonpath,''))
+			new_replacepath='/' + str(pathlib.Path(*p.parts[2:]))
+			new_replacepath=new_replacepath.replace('/.','')
+			#print(new_replacepath)
+			
+			
+			dest=out_folder + new_replacepath + '/' + str(filename)
+
+			#print(dest)
+            #remove extension(folder)
+					
+			dest, file_extension = os.path.splitext(dest)
+            
+            #split filename
+			repeated_times=filelist.count(filename)
+            #print(repeated_times)
+			 
+			filenametemp=filename.replace(file_extension,'') + '(' + str(repeated_times) + ')' + file_extension
+			dest=out_folder + new_replacepath
+			
+			copyfile(os.path.join(dirpath,filename), os.path.join(dest, filenametemp))
+			
+						
